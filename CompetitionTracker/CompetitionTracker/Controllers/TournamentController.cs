d4922c9c446098dbaa1d4345fbf7a3fe3cc58512
Michael <private1384@live.com>
2017-04-08 21:36:30 -0600

    Updates Tournament Controller, adds RoundRobin model and updates .gitignore to ignore Migrations folder

--- CompetitionTracker/CompetitionTracker/Controllers/TournamentController.cs
+++ CompetitionTracker/CompetitionTracker/Controllers/TournamentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CompetitionTracker.Models;
 using CompetitionTracker.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,7 +21,8 @@ namespace CompetitionTracker.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Competitor> compList =_DbContext.Competitors.ToList();
+            return View(compList);
         }
     }
 }
---
+++ CompetitionTracker/CompetitionTracker/Models/RoundRobin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionTracker.Models
+{
+    public class RoundRobin
+    {
+        public List<Competitor> compList;
+        public RoundRobin(List<Competitor> cList) {
+            compList = cList;
+        }
+
+
+    }
+}
